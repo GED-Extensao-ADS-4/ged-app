0fc162bd2c029d38bfd859bcc1fee66e1cfc747f
Douglas <douglasboss98@gmail.com>
2024-11-07 20:19:38 -0300

    Feat: Criadas as classes model de document e também o DTO e enums

---
+++ src/main/java/br/apae/ged/models/Document.java
@@ -0,0 +1,36 @@
+package br.apae.ged.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity(name = "tb_documentos")
+public class Document {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String tipoDocumento;
+    private String tipoArquivo;
+    private String path;
+    private LocalDateTime dataUpload;
+    private LocalDateTime dataDownload;
+    private String downloadedBy;
+    private String uploadedBy;
+    @OneToOne
+    @JoinColumn(name = "link_id", referencedColumnName = "id")
+    private Document prevVersion;
+    private Boolean isLast;
+
+    public Document(){
+        this.dataUpload = LocalDateTime.now();
+        this.isLast = true;
+    }
+}
