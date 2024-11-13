f5499359eaa51f1e6394dfd84648413eb6a27956
LEONARDO RODRIGUES DA COSTA <leonardo.costa1@alunos.sc.senac.br>
2024-11-13 20:59:58 -0300

    feat: repositories alunos

--- src/main/java/br/apae/ged/repositories/RepositoryAluno.java
+++ src/main/java/br/apae/ged/repositories/RepositoryAluno.java
@@ -1,4 +1,13 @@
 package br.apae.ged.repositories;
 
-public interface RepositoryAluno {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepositoryAluno extends JpaRepository <Aluno, Long> {
+    Aluno findByCpf(String cpf);
+
+    Aluno findByNome(String nome);
+
+    Aluno findByStatus(boolean is_ativo);
 }
