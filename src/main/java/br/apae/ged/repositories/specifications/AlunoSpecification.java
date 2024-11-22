7df43cf59b1ee4f218a0c0568859a938e49a7875
LucasRBeckhauser <lucasbeckh@live.com>
2024-11-21 21:18:47 -0300

    Feat: Criação do AlunoSpecification.

---
+++ src/main/java/br/apae/ged/repositories/specifications/AlunoSpecification.java
@@ -0,0 +1,35 @@
+package br.apae.ged.repositories.specifications;
+
+import br.apae.ged.models.Alunos;
+import org.springframework.data.jpa.domain.Specification;
+
+public class AlunoSpecification {
+
+    private AlunoSpecification(){
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static Specification<Alunos> isAtivo(){
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("isAtivo"), true);
+    }
+
+    public static Specification<Alunos> byNome(String nome) {
+        return (root, query, criteriaBuilder) -> {
+            if (nome == null || nome.isBlank() || nome.isEmpty()){
+                return criteriaBuilder.conjunction();
+            }
+            return criteriaBuilder.like(root.get("nome"), "%" + nome + "%");
+        };
+    }
+
+    public static Specification<Alunos> byCpf(String cpf) {
+        return (root, query, criteriaBuilder) -> {
+            if (cpf == null || cpf.isBlank() || cpf.isEmpty()) {
+                return criteriaBuilder.conjunction();
+            }
+            return criteriaBuilder.like(root.get("cpf"), "%" + cpf + "%");
+        };
+    }
+
+
+}
