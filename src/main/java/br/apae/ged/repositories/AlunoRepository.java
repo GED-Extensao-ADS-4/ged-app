f257fa0f9dfd520ac9e1837de6c966b483c8558e
LucasRBeckhauser <lucasbeckh@live.com>
2024-11-21 21:22:08 -0300

    Feat: Estendendo a interface JpaSpecificationExecutor para a entidade Alunos para a utilização de consultas dinâmicas com o Specification.

--- src/main/java/br/apae/ged/repositories/AlunoRepository.java
+++ src/main/java/br/apae/ged/repositories/AlunoRepository.java
@@ -2,10 +2,11 @@ package br.apae.ged.repositories;
 
 import br.apae.ged.models.Alunos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AlunoRepository extends JpaRepository <Alunos, Long> {
+public interface AlunoRepository extends JpaRepository <Alunos, Long>, JpaSpecificationExecutor<Alunos> {
     Alunos findByCpf(String cpf);
     Alunos findByRg(String rg);
 }
