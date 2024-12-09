c5bf2a2e24cfb7fc7242bb51964acb03cfe4ca5f
juan.zeferino <juancarlosbatistazeferino@gmail.com>
2024-12-09 20:30:03 -0300

    fix: removido exception específica

--- src/main/java/br/apae/ged/exceptions/AlunoNaoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package br.apae.ged.exceptions;
-
-public class AlunoNaoEncontradoException extends RuntimeException {
-    public AlunoNaoEncontradoException(Long id) {
-        super("Aluno com ID " + id + " não foi encontrado.");
-    }
-}
